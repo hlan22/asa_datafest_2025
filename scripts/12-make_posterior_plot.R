686aa2e49ac3faf5897b6d785ca91fdf8fb1c7b2
Caden Hewlett <thetroutmanishere@gmail.com>
2025-03-23 14:03:36 -0700

    grid of all posteriors

--- scripts/12-make_posterior_plot.R
+++ scripts/12-make_posterior_plot.R
@@ -1,4 +1,6 @@
 setwd(here::here())
+library(gridExtra)
+library(grid)
 df <- read.csv(here::here("additional_data", "clean", "final_data.csv"))
 all_fits <- readRDS(here::here("additional_data", "clean", "all_models.RData"))
 
@@ -85,7 +87,7 @@ coefs_long <- coefs_df %>%
     values_to = "value"
   )
 coefs_long$predictor <- factor(coefs_long$predictor, levels = var_order)
-ggplot(coefs_long, aes(x = value, y = reorder(predictor, value), fill = predictor)) +
+chicago_plot <- ggplot(coefs_long, aes(x = value, y = reorder(predictor, value), fill = predictor)) +
   stat_halfeye(
     .width = c(0.5, 0.95),
     point_interval = median_qi, 
@@ -100,3 +102,61 @@ ggplot(coefs_long, aes(x = value, y = reorder(predictor, value), fill = predicto
     y = "Predictor"
   ) +
   theme_bw() + theme(legend.position = "none")
+
+ggsave(filename = "outputs/posterior_chicago.png",
+       plot = chicago_plot,
+       width = 8,
+       height = 6
+)
+
+# ---------- ALL plots ---------- #
+plot_list <- list()
+
+for (mkt in markets) {
+  fit <- all_fits[[mkt]]
+  colnames(fit$beta_raw) <- var_order  # apply variable names
+  
+  coefs <- as.data.frame(fit$beta_raw)
+  coefs$iteration <- 1:nrow(coefs)
+  
+  coefs_long <- coefs %>%
+    pivot_longer(-iteration, names_to = "predictor", values_to = "value")
+  
+  coefs_long$predictor <- factor(coefs_long$predictor, levels = var_order)
+  
+  p <- ggplot(coefs_long, aes(x = value, y = predictor, fill = predictor)) +
+    stat_halfeye(
+      .width = c(0.5, 0.95),
+      point_interval = median_qi, 
+      slab_color = "black",
+      alpha = 0.6
+    ) +
+    scale_fill_viridis_d(option = "D") +
+    labs(
+      title = mkt,
+      subtitle = "Posterior Coefficient Distributions",
+      x = NULL,
+      y = NULL
+    ) +
+    theme_bw(base_size = 10) +
+    theme(legend.position = "none")
+  
+  plot_list[[mkt]] <- p
+}
+for (i in seq_along(plot_list)) {
+  if (!i %in% c(1, 4, 7)) {
+    plot_list[[i]] <- plot_list[[i]] +
+      theme(
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()
+      )
+  }
+}
+wrapped_plots <- lapply(plot_list, function(p) {
+  arrangeGrob(p, heights = unit(1, "null"), widths = unit(1, "null"))
+})
+grid_arranged <- do.call(grid.arrange, c(wrapped_plots, ncol = 3))
+png("outputs/posterior_grid.png", width = 1400, height = 1000, res = 150)
+grid.draw(grid_arranged)
+dev.off()
